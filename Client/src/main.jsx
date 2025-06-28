18df96356e6c61c60043963f5bf991e04785ee0b
Ritik Saini(dev-rishiz) <ritikksainii26@gmail.com>
2025-06-28 15:25:03 +0530

    first commit

---
+++ Client/src/main.jsx
@@ -0,0 +1,29 @@
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Login from "./pages/Authentication/Login.jsx";
+import Signup from "./pages/Authentication/Signup.jsx";
+import Home from "./pages/Home/Home.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    <App />
+    <RouterProvider router={router} />
+  </>
+);
---
+++ Client/src/pages/Authentication/Login.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import MyImage from "../../assets/side_img.jpg";
+
+const Login = () => {
+  return (
+    <div className="flex">
+      <div id="images" className=" w-1/2 h-[100vh] ">
+        <img src={MyImage} alt="icons" />
+      </div>
+      <div className="w-1/2 flex items-center justify-center flex-col  gap-4 p-4  border border-white">
+        <h1 className="text-2xl font-bold">Log in</h1>
+        <label className="input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+              <circle cx="12" cy="7" r="4"></circle>
+            </g>
+          </svg>
+          <input
+            type="text"
+            required
+            placeholder="Username"
+            pattern="[A-Za-z][A-Za-z0-9\-]*"
+            minLength="3"
+            maxLength="30"
+            title="Only letters, numbers or dash"
+          />
+        </label>
+        <p className="validator-hint hidden">
+          Must be 3 to 30 characters
+          <br />
+          containing only letters, numbers or dash
+        </p>
+        <div className="validator-hint hidden">Enter valid email address</div>
+
+        <label className="input validator">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+              <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+            </g>
+          </svg>
+          <input
+            type="password"
+            required
+            placeholder="Password"
+            minlength="8"
+            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+          />
+        </label>
+        <p className="validator-hint hidden">
+          Must be more than 8 characters, including
+          <br />
+          At least one number <br />
+          At least one lowercase letter <br />
+          At least one uppercase letter
+        </p>
+        <button className="btn btn-primary">Log in</button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
